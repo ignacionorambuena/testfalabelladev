8715c32c8dbf18cd053800d9c4e3b985562c5bb7
MacbookAir <macbookair@MacBook-Air-de-MacbookAir.local>
2019-11-01 02:32:31 -0300

    Se agregan reglas de evaluador de productos

--- models/evaluate-products.model.js
+++ models/evaluate-products.model.js
@@ -16,30 +16,149 @@ function getEvaluateProducts(days) {
     [Price]
     - Indica costo del producto
     - Disminuye 1 cuando cambia el día
-    - Nunca puede ser negativo 
-    - Cuando es < 0, el precio disminuye 2
-    - Nunca es mayor que 100 [!>100]
+    - Nunca puede ser negativo
+    - Nunca es mayor que 100
     - Queda en 0 cuando SellIn == 0
 
-    [Full Cobertura]
+    [Full Cobertura] OK
     - Aumenta su precio al cambiar el día
 
-    [Mega Cobertura]
+    [Mega Cobertura] OK
     - No disminuye días
     - No disminuye precio
     - Tiene un precio fijo de 180
 
-    [Super avance]
-    - Disminuye su precio el doble
+    [Super avance] 
+    - Disminuye su precio el doble +2
     
 
     [Full Cobertura Super Duper]
     - Aumenta su precio al cambiar el día
-    - Incrementa 2 cuando quedan 10 días o menos [sellIn <=10]
-    - Incrementa 3 cuando quedan 10 días o menos [sellIn <=5]
+    - Incrementa precio 2 cuando quedan 10 días o menos [sellIn <=10]
+    - Incrementa precio 3 cuando quedan 5 días o menos [sellIn <=5]
 
 
+    1:Cobertura
+    2:Full Cobertura *
+    3:Baja Cobertura
+    4:Mega Cobertura *
+    5:Full cobertura super duper *
+    6:Super avance *
     */
+    const data = [];
+    let temp = [];
+    for (let i = 0; i < parseInt(days); i++) {
+        if (temp.length === 0) {
+            console.log('------- dia ', i, '------');
+            console.log('id, nombre, sellIn, price');
+            products.map(prd => {
+                switch (prd.id) {
+                    case 1: // Cobertura
+                        temp.push({
+                            id: prd.id,
+                            nombre: prd.nombre,
+                            sellIn: prd.sellIn - 1,
+                            price: prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? prd.price == 0 ? 0 : prd.price - 2 : prd.price - 1
+                        });
+
+                        console.log(prd.id, ',', prd.nombre, ',', prd.sellIn - 1, ',', prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? prd.price == 0 ? 0 : prd.price - 2 : prd.price - 1);
+                        break;
+                    case 2: // Full cobertura
+                        temp.push({
+                            id: prd.id,
+                            nombre: prd.nombre,
+                            sellIn: prd.sellIn - 1,
+                            price: prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? prd.price == 0 ? 0 : prd.price - 2 : prd.price >= 100 ? 100 : prd.price + 1
+                        });
+                        console.log(prd.id, ',', prd.nombre, ',', prd.sellIn - 1, ',', prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? prd.price == 0 ? 0 : prd.price - 2 : prd.price >= 100 ? 100 : prd.price + 1);
+                        break;
+                    case 3: // Baja Cobertura
+                        temp.push({
+                            id: prd.id,
+                            nombre: prd.nombre,
+                            sellIn: prd.sellIn - 1,
+                            price: prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? prd.price == 0 ? 0 : prd.price - 2 : prd.price - 1
+                        });
+
+                        console.log(prd.id, ',', prd.nombre, ',', prd.sellIn - 1, ',', prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? prd.price == 0 ? 0 : prd.price - 2 : prd.price - 1);
+                        break;
+                    case 4: // Mega Cobertura
+                        temp.push({
+                            id: prd.id,
+                            nombre: prd.nombre,
+                            sellIn: prd.sellIn,
+                            price: 180
+                        });
+                        console.log(prd.id, ',', prd.nombre, ',', prd.sellIn, ',', 180);
+                        break;
+                    case 5: // Full cobertura super duper
+                        temp.push({
+                            id: prd.id,
+                            nombre: prd.nombre,
+                            sellIn: prd.sellIn - 1,
+                            price: prd.sellIn <= 10 && prd.sellIn > 5 && prd.price < 100 ? prd.price + 2 : prd.sellIn <= 5 && prd.sellIn > 0 && prd.price <= 100 ? prd.price + 3 : prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? 0 : prd.price >= 100 ? 100 : prd.price + 1
+                        });
+                        console.log(prd.id, ',', prd.nombre, ',', prd.sellIn - 1, ',', prd.sellIn <= 10 && prd.sellIn >= 5 && prd.price <= 100 ? prd.price + 2 : prd.sellIn < 5 && prd.sellIn > 0 && prd.price < 100 ? prd.price + 3 : prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? 0 : prd.price >= 100 ? 100 : prd.price + 1);
+                        break;
+                }
+            });
+            data.push({ dia: i, products: temp });
+        } else {
+            console.log('------- dia ', i, '------');
+            console.log('id, nombre, sellIn, price');
+            temp.map(function (prd, idx) {
+                switch (prd.id) {
+                    case 1: // Cobertura
+                        temp[idx] = {
+                            id: prd.id,
+                            nombre: prd.nombre,
+                            sellIn: prd.sellIn - 1,
+                            price: prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? prd.price == 0 ? 0 : prd.price - 2 : prd.price - 1
+                        };
+                        console.log(prd.id, ',', prd.nombre, ',', prd.sellIn - 1, ',', prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? prd.price == 0 ? 0 : prd.price - 2 : prd.price - 1);
+                        break;
+                    case 2: // Full cobertura
+                        temp[idx] = {
+                            id: prd.id,
+                            nombre: prd.nombre,
+                            sellIn: prd.sellIn - 1,
+                            price: prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? prd.price == 0 ? 0 : prd.price - 2 : prd.price >= 100 ? 100 : prd.price + 1
+                        };
+                        console.log(prd.id, ',', prd.nombre, ',', prd.sellIn - 1, ',', prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? prd.price == 0 ? 0 : prd.price - 2 : prd.price >= 100 ? 100 : prd.price + 1);
+                        break;
+                    case 3: // Baja Cobertura
+                        temp[idx] = {
+                            id: prd.id,
+                            nombre: prd.nombre,
+                            sellIn: prd.sellIn - 1,
+                            price: prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? prd.price == 0 ? 0 : prd.price - 2 : prd.price - 1
+                        };
+                        console.log(prd.id, ',', prd.nombre, ',', prd.sellIn - 1, ',', prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? prd.price == 0 ? 0 : prd.price - 2 : prd.price - 1);
+                        break;
+                    case 4: // Mega Cobertura
+                        temp[idx] = {
+                            id: prd.id,
+                            nombre: prd.nombre,
+                            sellIn: prd.sellIn,
+                            price: 180
+                        };
+                        console.log(prd.id, ',', prd.nombre, ',', prd.sellIn, ',', 180);
+                        break;
+                    case 5: // Full cobertura super duper
+                        temp[idx] = {
+                            id: prd.id,
+                            nombre: prd.nombre,
+                            sellIn: prd.sellIn - 1,
+                            price: prd.sellIn <= 10 && prd.sellIn >= 5 && prd.price < 100 ? prd.price + 2 : prd.sellIn <= 5 && prd.sellIn > 0 && prd.price <= 100 ? prd.price + 3 : prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? 0 : prd.price >= 100 ? 100 : prd.price + 1
+                        };
+                        console.log(prd.id, ',', prd.nombre, ',', prd.sellIn - 1, ',', prd.sellIn <= 10 && prd.sellIn >= 5 && prd.price <= 100 ? prd.price + 2 : prd.sellIn < 5 && prd.sellIn > 0 && prd.price < 100 ? prd.price + 3 : prd.sellIn == 0 ? 0 : prd.sellIn < 0 ? 0 : prd.price >= 100 ? 100 : prd.price + 1);
+                        break;
+                }
+            });
+            data.push({ dia: i, products: temp });
+        }
+    }
+
     return new Promise((resolve, reject) => {
         if (products.length === 0) {
             reject({
@@ -48,7 +167,7 @@ function getEvaluateProducts(days) {
             })
         }
 
-        resolve(products)
+        resolve('Imprimiendo resultado en logs')
     })
 }
 
